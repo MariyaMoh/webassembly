af509503997600017ddb043a20c8137ba9a52779
MariyaMoh <alshaibi199732@gmail.com>
2022-06-10 20:15:34 +0200

    Maryia

---
+++ public/wasm/release.d.ts
@@ -0,0 +1,13 @@
+/**
+ * assembly/index/ticTacToeResult
+ * @param board `~lib/array/Array<i32>`
+ * @returns `i32`
+ */
+export declare function ticTacToeResult(board: Array<number>): number;
+/**
+ * assembly/index/add
+ * @param a `i32`
+ * @param b `i32`
+ * @returns `i32`
+ */
+export declare function add(a: number, b: number): number;
